9f091b270b7ab323141734fd4396e9975a464143
Naveen <neevanreddy8796@gmail.com>
2024-08-21 12:25:45 -0400

    commit 1

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+public static void main(String a[]){
+    int num1= 48;
+    double num2=12.3;
+    boolean result = num1==num2;
+
+
+    System.out.println(result);
+
+}
+}
